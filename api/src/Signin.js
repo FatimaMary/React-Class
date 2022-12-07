f0b2dbf14870e006829bee94dca316334d220d57
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2022-12-07 11:40:52 +0530

    Using Api

--- api/src/Signin.js
+++ api/src/Signin.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 export default function Signin() {
+    const [email, setEmail] = useState([]);
+    const [password, setPassword] = useState([]);
+    const [data, setData] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios 
+        .get("http://127.0.0.1:8000/students/")
+        .then(res => this.setData({ data: res.data }))
+        .catch(err => console.log(err));
+    }
+
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Email:</label>
-            <input placeholder='Email'></input>
+            <input placeholder='Email' value={email} onChange={(e)=> setEmail(e.target.value)}></input>
             <label>Password:</label>
-            <input placeholder='Password'></input>
+            <input placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}></input>
+            <button>Submit</button>
         </form>
     </div>
   )
