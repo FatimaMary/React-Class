3585850ed4ecc5396fd542ecce6d250f91bce965
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2022-12-07 11:17:10 +0530

    updated

---
+++ api/src/Signin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Signin() {
+  return (
+    <div>
+        <form>
+            <label>Email:</label>
+            <input placeholder='Email'></input>
+            <label>Password:</label>
+            <input placeholder='Password'></input>
+        </form>
+    </div>
+  )
+}
